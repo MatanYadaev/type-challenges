c428e44470322e7ef3d39042973a3abb04094116
Matan Yadaev <matan.yed@gmail.com>
2022-03-23 00:48:05 +0200

    answer 108-trim

--- questions/108-medium-trim/template.ts
+++ questions/108-medium-trim/template.ts
@@ -1,1 +1,10 @@
-type Trim<S extends string> = any
+type TrimTuple = ' ' | '\t' | '\n'
+
+type Trim<String extends string> =
+    String extends `${TrimTuple}${infer Pre}`
+      ? Trim<Pre>
+      : (
+        String extends `${infer Post}${TrimTuple}`
+          ? Trim<Post>
+          : String
+      )
